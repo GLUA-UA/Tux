e9c42edbcc074b5b80802fd91e39c4b2cb2dc8b9
Joao-Felisberto <58549684+Joao-Felisberto@users.noreply.github.com>
2023-10-15 17:02:24 +0100

    Customizable channel name roles (#4)
    * Added channel counter for guild members
    
    * Added channel counter for GLUA members
    
    * Updated .env.example
    
    * Auto role
    
    * Code style compliance
    
    * Fixed copy paste errors
    
    * Fixed auto role dependence on the member count channel
    
    * Added customizable count channel names
    
    ---------
    
    Signed-off-by: Joao-Felisberto <58549684+Joao-Felisberto@users.noreply.github.com>

--- src/discord/events/guildMemberAdd.ts
+++ src/discord/events/guildMemberAdd.ts
@@ -3,6 +3,8 @@ import Logger from "../../Logger";
 
 const MEMBER_CNT_CHANNEL: string = process.env.MEMBER_CNT_CHANNEL ?? "";
 const AUTO_ROLE: string = process.env.AUTO_ROLE ?? "";
+const GLUA_CNT_STRING: string = process.env.GLUA_CNT_STRING ?? "";
+const MEMBER_CNT_STRING: string = process.env.MEMBER_CNT_STRING ?? "";
 
 export async function run(client: Client, member: GuildMember) {
     updateMemberCount(member);
@@ -20,13 +22,13 @@ async function updateMemberCount(member: GuildMember) {
 
     if (c == null) return;
 
-    Logger.debug(`Membros: ${humanMembers}`);
-    c.setName(`Membros: ${humanMembers}`);
+    Logger.debug(MEMBER_CNT_STRING.replace("{n}", `${humanMembers}`));
+    c.setName(MEMBER_CNT_STRING.replace("{n}", `${humanMembers}`));
 }
 
 async function giveAutoRole(member: GuildMember) {
     if (AUTO_ROLE == "") return;
 
-    member.roles.add(await member.guild.roles.fetch(AUTO_ROLE) ?? "")
+    member.roles.add(await member.guild.roles.fetch(AUTO_ROLE) ?? "");
 }
 
--- src/discord/events/ready.ts
+++ src/discord/events/ready.ts
@@ -4,6 +4,8 @@ import Logger from "../../Logger";
 const GLUA_ROLE: string = process.env.GLUA_ROLE ?? "";
 const GLUA_CNT_CHANNEL: string = process.env.GLUA_CNT_CHANNEL ?? "";
 const MEMBER_CNT_CHANNEL: string = process.env.MEMBER_CNT_CHANNEL ?? "";
+const GLUA_CNT_STRING: string = process.env.GLUA_CNT_STRING ?? "";
+const MEMBER_CNT_STRING: string = process.env.MEMBER_CNT_STRING ?? "";
 
 export async function run(client: Client) {
     client.user?.setPresence({ activities: [{ name: "SuperTux", type: ActivityType.Playing }] });
@@ -21,13 +23,14 @@ export async function run(client: Client) {
 
     if (c == null) return;
 
-    Logger.debug(`GLUA: ${gluaMembers}`);
-    c.setName(`GLUA: ${gluaMembers}`);
+    Logger.debug(GLUA_CNT_STRING.replace("{n}", `${gluaMembers}`));
+    c.setName(GLUA_CNT_STRING.replace("{n}", `${gluaMembers}`));
+
 
     const c1 = await guild.channels.fetch(MEMBER_CNT_CHANNEL);
 
     if (c1 == null) return;
 
-    Logger.debug(`Membros: ${humanMembers}`);
-    c1.setName(`Membros: ${humanMembers}`);
+    Logger.debug(MEMBER_CNT_STRING.replace("{n}", `${humanMembers}`));
+    c1.setName(MEMBER_CNT_STRING.replace("{n}", `${humanMembers}`));
 }
