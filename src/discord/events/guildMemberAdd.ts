aefdeeb2253e4ed69309fa7c82468b45b191900b
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2023-10-15 18:00:17 +0100

    Add logging on auto role

--- src/discord/events/guildMemberAdd.ts
+++ src/discord/events/guildMemberAdd.ts
@@ -1,14 +1,14 @@
 import { Client, GuildMember } from "discord.js";
+import Logger from "../../Logger";
 
-const AUTO_ROLE: string = process.env.AUTO_ROLE ?? "";
+const AUTO_ROLE = process.env.AUTO_ROLE;
 
 export async function run(client: Client, member: GuildMember) {
     giveAutoRole(member);
 }
 
 async function giveAutoRole(member: GuildMember) {
-    if (AUTO_ROLE == "") return;
-
-    member.roles.add(await member.guild.roles.fetch(AUTO_ROLE) ?? "");
+    if (!AUTO_ROLE) return;
+    Logger.debug(`Giving ${member.user.tag} (${member.user.id}) the auto role`);
+    member.roles.add(AUTO_ROLE);
 }
-
