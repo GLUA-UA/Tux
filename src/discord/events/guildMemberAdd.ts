a968cba35c867c5e6b3d864ee2ddd842c0d09266
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2023-10-15 17:58:09 +0100

    Change the way the member channels are updated

--- src/discord/events/guildMemberAdd.ts
+++ src/discord/events/guildMemberAdd.ts
@@ -1,29 +1,9 @@
-import { Client, GuildBasedChannel, GuildMember, Role } from "discord.js";
-import Logger from "../../Logger";
+import { Client, GuildMember } from "discord.js";
 
-const MEMBER_CNT_CHANNEL: string = process.env.MEMBER_CNT_CHANNEL ?? "";
 const AUTO_ROLE: string = process.env.AUTO_ROLE ?? "";
-const GLUA_CNT_STRING: string = process.env.GLUA_CNT_STRING ?? "";
-const MEMBER_CNT_STRING: string = process.env.MEMBER_CNT_STRING ?? "";
 
 export async function run(client: Client, member: GuildMember) {
-    updateMemberCount(member);
-
     giveAutoRole(member);
-}
-
-async function updateMemberCount(member: GuildMember) {
-    const members = await member.guild?.members.fetch();
-    const humanMembers = members?.filter((m: GuildMember) => !m.user.bot).size;
-
-    Logger.debug(`New discord member: ${member.displayName}. The server has ${humanMembers} humans.`);
-
-    const c = await member.guild.channels.fetch(MEMBER_CNT_CHANNEL);
-
-    if (c == null) return;
-
-    Logger.debug(MEMBER_CNT_STRING.replace("{n}", `${humanMembers}`));
-    c.setName(MEMBER_CNT_STRING.replace("{n}", `${humanMembers}`));
 }
 
 async function giveAutoRole(member: GuildMember) {
--- src/discord/events/ready.ts
+++ src/discord/events/ready.ts
@@ -1,36 +1,11 @@
-import { ActivityType, Client, GuildBasedChannel, GuildMember } from "discord.js";
+import { ActivityType, Client } from "discord.js";
 import Logger from "../../Logger";
-
-const GLUA_ROLE: string = process.env.GLUA_ROLE ?? "";
-const GLUA_CNT_CHANNEL: string = process.env.GLUA_CNT_CHANNEL ?? "";
-const MEMBER_CNT_CHANNEL: string = process.env.MEMBER_CNT_CHANNEL ?? "";
-const GLUA_CNT_STRING: string = process.env.GLUA_CNT_STRING ?? "";
-const MEMBER_CNT_STRING: string = process.env.MEMBER_CNT_STRING ?? "";
+import { MemberCounterHelper } from "../MemberCountHelper";
 
 export async function run(client: Client) {
     client.user?.setPresence({ activities: [{ name: "SuperTux", type: ActivityType.Playing }] });
-
+    
     Logger.log(`Bot Ready! Logged in as ${client.user?.tag ?? "unknown"}`);
 
-    const guild = client.guilds.cache.at(0);
-    if (guild == null) return;
-
-    const members = await guild.members.fetch();
-    const gluaMembers = members?.filter((m: GuildMember) => m.roles.cache.has(GLUA_ROLE)).size;
-    const humanMembers = members?.filter((m: GuildMember) => !m.user.bot).size;
-
-    const c = await guild.channels.fetch(GLUA_CNT_CHANNEL);
-
-    if (c == null) return;
-
-    Logger.debug(GLUA_CNT_STRING.replace("{n}", `${gluaMembers}`));
-    c.setName(GLUA_CNT_STRING.replace("{n}", `${gluaMembers}`));
-
-
-    const c1 = await guild.channels.fetch(MEMBER_CNT_CHANNEL);
-
-    if (c1 == null) return;
-
-    Logger.debug(MEMBER_CNT_STRING.replace("{n}", `${humanMembers}`));
-    c1.setName(MEMBER_CNT_STRING.replace("{n}", `${humanMembers}`));
+    new MemberCounterHelper(client).start();
 }
